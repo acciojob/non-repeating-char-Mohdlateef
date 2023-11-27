452c114f3a19b84921f2ee2044871f38c2fce1b6
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-27 16:14:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let flag=0;
+	for (let i = 0;i<str.length-1;i++) {
+		{
+			for(let j=i+1;j<str.length;i++)
+				{
+					if(str.charAt[i]==str.charAt[j])
+					{
+						flag=1;
+						break;
+					}
+				}
+			if(flag==0)
+			{
+				return str.charAt[i];
+			}
+		}
+		return null;
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
